0f6838868ae56513ee85520299383ff70093198b
Yair Perman <yair.perman@gmail.com>
2017-07-24 00:30:47 +0300

    improved board specs

--- test/board.spec.js
+++ test/board.spec.js
@@ -3,22 +3,23 @@ describe('Board', () => {
   afterEach(() => sandbox.restore())
 
   context('trello board exists', () => {
+    let listCards = [a`.list-cards`, a`.list-cards`]
+
     beforeEach(() =>
       bodyHas(
         a`#content`.with(
           a`.board-wrapper`.with(
-            a`#board`.with(
-              a`.list-cards`,
-              a`.list-cards` )))
+            a`#board`.with(...listCards)))
       )
     )
 
-    it('builds a lists', (done) => {
+    it('builds lists', (done) => {
       let CardsList = spy(global, 'CardsList')
       new Board()
 
       _.in(0, () => {
-        expect(CardsList).to.have.been.calledTwice
+        expect(CardsList.firstCall ).to.have.been.calledWith(listCards[0])
+        expect(CardsList.secondCall).to.have.been.calledWith(listCards[1])
         done()
       })
     })
@@ -26,6 +27,7 @@ describe('Board', () => {
 
   context('trello board not yet loaded', () => {
     let contentElement = a`#content`
+    let listCards
 
     beforeEach(() => bodyHas(contentElement))
 
@@ -33,34 +35,29 @@ describe('Board', () => {
       let CardsList = spy(global, 'CardsList')
       new Board()
 
-      contentElement.appendChild(a`.board-wrapper`.with(
-                                    a`.list-cards`,
-                                    a`.list-cards`))
+      let listCards = [a`.list-cards`, a`.list-cards`]
+      contentElement.appendChild(a`.board-wrapper`.with(...listCards))
 
       _.in(10, () => {
-        expect(CardsList).to.have.been.calledTwice
+        expect(CardsList.firstCall ).to.have.been.calledWith(listCards[0])
+        expect(CardsList.secondCall).to.have.been.calledWith(listCards[1])
         done()
       })
     })
   })
 
   context('trello board switch', () => {
-    let fireContentReloaded
-    let BoardSpy
-
-    let content      = a`#content`
-    let boardWrapper = a`.board-wrapper`.with(
-                          a`.list-cards`,
-                          a`.list-cards`)
-
-    beforeEach(() => {
-      bodyHas(
-        content.with(boardWrapper)
-      )
-      CardsList = spy(global, 'CardsList')
-    })
-
     it('builds a new board representation', (done) => {
+      let listCards = [ a`.list-cards`,
+                        a`.list-cards` ]
+
+      let boardWrapper = a`.board-wrapper`.with(...listCards)
+      let content      = a`#content`
+
+      bodyHas(content.with(boardWrapper))
+
+      let CardsList = spy(global, 'CardsList')
+
       new Board()
 
       content.innerHTML = ''
@@ -72,7 +69,8 @@ describe('Board', () => {
       })
 
       _.in(20, () => {
-        expect(CardsList).to.have.been.called
+        expect(CardsList.firstCall ).to.have.been.calledWith(listCards[0])
+        expect(CardsList.secondCall).to.have.been.calledWith(listCards[1])
         done()
       })
     })
